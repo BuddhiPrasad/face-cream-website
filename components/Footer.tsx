9d88266d25999c4ab9baf855e789fa1543342f01
Buddhi Prasad <bp980930@gmail.com>
2024-08-02 17:43:10 +0530

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import MaxWrapperContainer from './MaxWrapperContainer'
+import Logo from './Logo'
+import Social from './Social'
+
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='bg-secondary py-12'>
         <MaxWrapperContainer>
-            Footer
+            <div className='flex flex-col items-center gap-y-6'>
+                {/*Logo */}
+                <Logo/>
+                {/*Social */}
+                <Social/>
+                {/*Copyright */}
+                <div className='text-muted-foreground'>
+                    Copyright &copy; Suyaama.co. All rights reserved.
+                </div>
+            </div>
         </MaxWrapperContainer>
       
     </footer>
